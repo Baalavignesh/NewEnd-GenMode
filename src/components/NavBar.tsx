646938fb7f81975e50a34804c1da90af9efd7c47
Baalavignesh <baalavignesh21@gmail.com>
2024-01-27 00:01:57 +0530

    title

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ const Header = () => {
 
   return (
     <div className='p-4 pl-24 pr-24 bg-custom-background text-custom-white flex justify-between items-center'>
-      <h2 onClick={() => navigate("/")} className="cursor-pointer">NewEnd</h2>
+      <h2 onClick={() => navigate("/")} className="cursor-pointer">NewEnd - GenMode</h2>
       <h4 onClick={() => navigate("/how")} className="cursor-pointer">How it works?</h4>
     </div>
   )
